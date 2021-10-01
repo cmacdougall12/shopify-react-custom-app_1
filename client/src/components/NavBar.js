ffd4ad96b0931c775e5d21b6fa1d8e3a43da6357
cmacdougall12 <cammacdougall12@gmail.com>
2021-10-01 13:58:38 -0600

    responisive navBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,7 +6,7 @@ export default function NavBar() {
   const { openCart } = useContext(ShopContext);
 
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="/home">
           <img
@@ -16,30 +16,33 @@ export default function NavBar() {
             alt="sc-logo"
           />
         </Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="/home">Home</Nav.Link>
-          <Nav.Link href="/home">Collections</Nav.Link>
-          <Nav.Link href="#account">Account</Nav.Link>
-        </Nav>
-        <Nav className="ml-auto">
-          <Nav.Link>
-            <img
-              className="p-0.5"
-              width={30}
-              src="/images/chat.svg"
-              alt="sc-logo"
-            />
-          </Nav.Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/home">Collections</Nav.Link>
+            <Nav.Link href="#account">Account</Nav.Link>
+          </Nav>
+          <Nav className="ml-auto">
+            <Nav.Link>
+              <img
+                className="p-0.5"
+                width={30}
+                src="/images/chat.svg"
+                alt="sc-logo"
+              />
+            </Nav.Link>
 
-          <Nav.Link onClick={openCart}>
-            <img
-              className="p-0.5"
-              width={30}
-              src="/images/shopping-cart-solid.svg"
-              alt="sc-logo"
-            />
-          </Nav.Link>
-        </Nav>
+            <Nav.Link onClick={openCart}>
+              <img
+                className="p-0.5"
+                width={30}
+                src="/images/shopping-cart-solid.svg"
+                alt="sc-logo"
+              />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
