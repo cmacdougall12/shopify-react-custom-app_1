ae266ec9ee08a2865a8f61caa677228d477343f6
cmacdougall12 <cammacdougall12@gmail.com>
2021-10-13 13:45:22 -0600

    install dependancies for socket.io

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import ShopProvider from "../context/ShopContext";
 import HomePage from "../pages/HomePage";
 import ProductPage from "../pages/ProductPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { SocketProvider } from "../context/SocketProvider";
 
 import NavBar from "./NavBar";
 import { ConversationsProvider } from "../context/ConversationsProvider";
@@ -12,25 +13,27 @@ import Register from "../pages/Register";
 function App() {
   return (
     <Router>
-      <ShopProvider>
-        <ConversationsProvider>
-          <NavBar />
-          <Switch>
-            <Route path="/product/:id">
-              <ProductPage />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route path="/">
-              <HomePage />
-            </Route>
-          </Switch>
-        </ConversationsProvider>
-      </ShopProvider>
+      <SocketProvider>
+        <ShopProvider>
+          <ConversationsProvider>
+            <NavBar />
+            <Switch>
+              <Route path="/product/:id">
+                <ProductPage />
+              </Route>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/register">
+                <Register />
+              </Route>
+              <Route path="/">
+                <HomePage />
+              </Route>
+            </Switch>
+          </ConversationsProvider>
+        </ShopProvider>
+      </SocketProvider>
     </Router>
   );
 }
